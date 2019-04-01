98325eaa08b36238e193637bc7f0fa71789deff0
Warleson Candido da Silva <warllesoncandido@gmail.com>
2019-04-01 09:57:49 -0300

    commit numero 2

--- js/scripit.js
+++ js/scripit.js
@@ -1,5 +1,37 @@
+
+function buscandoCep(){
+
+    const cep = document.getElementById("cep").value;
+
+
+    fetch(`http://viacep.com.br/ws/${cep}/json`)
+    .then(response =>{
+
+        return response.json();
+
+})
+.then(data => {
+    var cidade = document.getElementById("cidade");
+    var bairro = document.getElementById("bairro");
+    var rua = document.getElementById("rua");
+    var uf = document.getElementById("uf");
+    
+    cidade.value=data.localidade;
+    bairro.value = data.bairro;
+    rua.value = data.logradouro;
+    uf.value = data.uf;
+    
+    
+})
+
+
+};
+
 document.getElementById("imprimir").addEventListener("click",function(){
 
-    window.print();
-
+    var curriculo = document.write("Warleson Candido da Silva");
+    curriculo += "Nascido e Criado no Gueto";
+    
 });
+
+
